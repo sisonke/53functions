cb0d3b02e4c5912860ecef5bec12887a68f22937
sisonke <culoe2@gmail.com>
2015-07-08 15:30:30 +0200

    test changed

--- tests.js
+++ tests.js
@@ -29,7 +29,7 @@ QUnit.test( "upper test ", function( assert ) {
 });
 
 QUnit.test( "reverse test ", function( assert ) {
- assert.equal(reverse("eknosis"),"sisonke");
+ assert.equal(reverse("ekno"),"sisonke");
 });
 
 
